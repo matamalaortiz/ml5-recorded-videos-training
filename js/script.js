b957d15f827ea8940521e63605bdd287f4bab834
Alejandro <matamala@material.cl>
2018-02-28 10:53:22 -0500

    doc

--- js/script.js
+++ js/script.js
@@ -1,82 +1,92 @@
 "use strict";
 
-let videosArray = ["./videos/nocam.mp4", "./videos/white.mp4", "./videos/webcam.mp4"]; // your videos
-let videoHidden = document.getElementById('videoHidden'); // ml5 input video
-let videoLive = document.getElementById('videoLive'); // ux video
+let videosArray = ["./videos/nocam.mp4", "./videos/white.mp4", "./videos/webcam.mp4"]; //  videos array.
+let videoHidden = document.getElementById('videoHidden'); // ml5 input video 227px
+let videoLive = document.getElementById('videoLive'); // ux video, to use custom sizes.
 let videoTeach = document.getElementById('videoTraining'); // video tag for training
-let prediction = document.getElementById('prediction'); // prediction output
+let prediction = document.getElementById('prediction'); // prediction output.
 
 document.addEventListener('DOMContentLoaded', function() {
 
-let knn = new ml5.KNNImageClassifier(loopVideos, 2, 1);
-let index = 0;
-let trainingTime = 10;
-let startPred = false;
+  let knn = new ml5.KNNImageClassifier(loopVideos, 2, 1);
+  let index = 0;
+  let trainingTime = 10;
+  let startPred = false;
 
 
-function loopVideos(){
 
-  if(!startPred){
+  // Loop for videos in the array as soon as the page loads.
+  function loopVideos() {
 
-    let times = 0;
-    videoTeach.src = videosArray[index];
+    if (!startPred) {
 
-    videoTeach.onloadeddata = function() {
+      let times = 0;
+      videoTeach.src = videosArray[index];
 
-      let startTrain = setInterval(function(){
+      videoTeach.onloadeddata = function() {
 
-        knn.addImage(videoTeach, index+1);
-        prediction.innerText = `Training video # ${index+1}`
+        let startTrain = setInterval(function() {
 
-        times++;
-        stopTraining()
+          knn.addImage(videoTeach, index + 1);
+          prediction.innerText = `Training video # ${index+1}`
 
-      }, 150);
+          times++;
+          stopTraining()
 
-      function stopTraining() {
+        }, 150);
 
-        if(times === trainingTime){
-          clearInterval(startTrain);
-          index++;
+        function stopTraining() {
 
-          if(index < videosArray.length){
-            loopVideos();
-          } else {
+          if (times === trainingTime) {
+            clearInterval(startTrain);
+            index++; 
 
-            console.log('start predicting');
-
-            startPred = true;
-            window.startPred = startPred;
-
-            setInterval(function() {
-              knn.predict(videoHidden, function(data) {
-
-                let state = data.classIndex;
-
-                if(state === 1){
-                  prediction.innerText = `Covering the webcam with your hands.`;
-                } else if (state === 2) {
-                  prediction.innerText = `Covering the webcam with a white sheet of paper.`;
-                } else {
-                  prediction.innerText = `You are not covering your camera`;
-                }
-
-              })
-            }, 250);
+            if (index < videosArray.length) {
+              loopVideos();
+            } else {
+              startPrediction();
+            }
 
           }
         }
       }
     }
   }
-}
+
+  // After looping, start prediction.
+
+  function startPrediction() {
+    console.log('start predicting');
+
+    startPred = true;
+    window.startPred = startPred;
+
+    setInterval(function() {
+      knn.predict(videoHidden, function(data) {
+
+        let state = data.classIndex;
+
+        if (state === 1) {
+          prediction.innerText = `Covering the webcam with your hands.`;
+        } else if (state === 2) {
+          prediction.innerText = `Covering the webcam with a white sheet of paper.`;
+        } else {
+          prediction.innerText = `You are not covering your camera`;
+        }
+
+      })
+    }, 250);
+  }
 
 
-//Camera
+  // Camera Access.
   navigator.getUserMedia = navigator.getUserMedia;
 
   if (navigator.getUserMedia) {
-    navigator.getUserMedia({ audio: false, video: true },
+    navigator.getUserMedia({
+        audio: false,
+        video: true
+      },
       function(stream) {
         videoHidden.srcObject = stream;
         videoLive.srcObject = stream;
